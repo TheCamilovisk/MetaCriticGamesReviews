88b8fc787d5238e27061fb4472dcdbbfbca6246e
TheCamilovisk <camilolgon@gmail.com>
2024-02-21 19:20:20 -0300

    Add type hints.

--- tests/test_game_description.py
+++ tests/test_game_description.py
@@ -8,7 +8,7 @@ from tests.utils import get_resources_path, read_file
 
 
 class TestGetGameInfo(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.resources_path = get_resources_path(__file__)
         # Setup mock HTML content for different scenarios
         mock_valid_game_path = os.path.join(
@@ -25,7 +25,7 @@ class TestGetGameInfo(unittest.TestCase):
         self.incomplete_game_html = read_file(mock_incomplete_game_path)
 
     @patch("scraping.extraction.requests.get")
-    def test_valid_game_page(self, mock_get):
+    def test_valid_game_page(self, mock_get: MagicMock) -> None:
         """Test extracting info from a valid game page URL."""
         mock_get.return_value = MagicMock(status_code=200, text=self.valid_game_html)
         game_info = get_game_info("https://www.notmetacritic.com/game/valid-game")
@@ -33,20 +33,20 @@ class TestGetGameInfo(unittest.TestCase):
         self.assertIsInstance(game_info, GameInfo)
 
     @patch("scraping.extraction.requests.get")
-    def test_invalid_url_format(self, mock_get):
+    def test_invalid_url_format(self, mock_get: MagicMock) -> None:
         """Test handling of invalid URL format."""
         with self.assertRaises(ValueError):
             get_game_info("https://www.notmetacritic.com/game/invalid-url")
 
     @patch("scraping.extraction.requests.get")
-    def test_non_existent_game_page(self, mock_get):
+    def test_non_existent_game_page(self, mock_get: MagicMock) -> None:
         """Test handling of non-existent game page URL."""
         mock_get.return_value = MagicMock(status_code=404, text=self.missing_game_html)
         with self.assertRaises(ValueError):
             get_game_info("https://www.notmetacritic.com/game/non-existent-game")
 
     @patch("scraping.extraction.requests.get")
-    def test_empty_or_incomplete_game_page(self, mock_get):
+    def test_empty_or_incomplete_game_page(self, mock_get: MagicMock) -> None:
         """Test handling of empty or incomplete game page."""
         mock_get.return_value = MagicMock(
             status_code=200, text=self.incomplete_game_html
