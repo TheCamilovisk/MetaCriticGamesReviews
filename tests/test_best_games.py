d2dd39e35ec325f58a41146acf267195685199d1
TheCamilovisk <camilolgon@gmail.com>
2024-02-20 21:12:18 -0300

    Initial basic test structure for best_games test case.

---
+++ tests/test_best_games.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+from unittest.mock import MagicMock, patch
+
+from scraping.extraction import get_best_games_list
+from tests.utils import get_resources_path, read_file
+
+
+class TestBestGames(unittest.TestCase):
+    def setUp(self) -> None:
+        self.resources_path = get_resources_path(__file__)
+
+        mock_page_path = os.path.join(self.resources_path, "mock_page.html")
+        self.mock_page_html = read_file(mock_page_path)
+
+    """
+    Unit tests for the `get_best_games_list` function in the `scraping` module.
+    """
+
+    @patch("scraping.extraction.requests.get")
+    def test_get_games_list_success(self, mock_get: MagicMock) -> None:
+        """
+        Tests successful extraction of games listing (hrefs) from a mocked
+        webpage.
+        """
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.text = self.mock_page_html
+
+        expected_links = [
+            "https://example.com/game1",
+            "https://example.com/game2",
+        ]
+        links = get_best_games_list()
+
+        self.assertEqual(links, expected_links)
---
+++ tests/utils.py
@@ -0,0 +1,32 @@
+import os
+
+
+def get_resources_path(test_file_name: str) -> str:
+    """Constructs the path to the resources folder of the test case based on
+    the current file name.
+
+    Args:
+        test_file_name (str): The name of the test file
+
+    Returns:
+        str: The full path to the resources folder
+    """
+    basename = os.path.basename(test_file_name)
+    filename = os.path.splitext(basename)[0]
+    foldername = filename.removeprefix("test_")
+    basepath = os.path.dirname(test_file_name)
+    return os.path.join(basepath, foldername)
+
+
+def read_file(filepath: str) -> str:
+    """Return the contents of a raw text file.
+
+    Args:
+        filepath (str): The path to the raw text file
+
+    Returns:
+        str: The raw text file contents
+    """
+    with open(filepath, "r") as file:
+        content = file.read()
+    return content
