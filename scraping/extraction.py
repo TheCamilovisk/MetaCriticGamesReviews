1d83d1c2e335d73321276df9d40515dd3c97c7b4
TheCamilovisk <camilolgon@gmail.com>
2024-02-20 21:48:41 -0300

    Removing unnecessary comment.

--- scraping/extraction.py
+++ scraping/extraction.py
@@ -8,8 +8,6 @@ from scraping.models import GameURL
 def get_best_games_list(n_pages: int = 1) -> List[GameURL]:
     """Get the list of the best games of all time for the "n_pages" first pages in https://www.metacritic.com/browse/game/.  # noqa: E501
 
-    Each page contains a list of at most 24 games. So, if we want to get the list for 4 pages, we'll have 96 game URLS.
-
     Args:
         n_pages (int, optional): The desired number of pages to retrieve the game URLS from. If < 0, all pages will be scraped. Defaults to 1.
 
