e67e3fc609f010d482c20817aed0903d63705872
TheCamilovisk <camilolgon@gmail.com>
2024-02-20 21:46:00 -0300

    Enhancing docstrings.

--- scraping/models.py
+++ scraping/models.py
@@ -3,5 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class GameURL:
+    """Model to represent the mapping between a game title and it's page URL.
+    """
     title: str
     url: str
