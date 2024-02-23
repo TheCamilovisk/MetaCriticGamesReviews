624e72bc7f11bd1b2666e205eaf4112553e9a072
TheCamilovisk <camilolgon@gmail.com>
2024-02-23 19:33:29 -0300

    Fixing games descriptions tests.

--- tests/test_game_description.py
+++ tests/test_game_description.py
@@ -2,6 +2,8 @@ import os
 import unittest
 from unittest.mock import MagicMock, patch
 
+import requests
+
 from scraping.extraction import get_game_info
 from scraping.models import GameInfo
 from tests.utils import get_resources_path, read_file
@@ -15,6 +17,10 @@ class TestGetGameInfo(unittest.TestCase):
             self.resources_path, "mock_valid_game_basic_info_page.html"
         )
         self.valid_game_html = read_file(mock_valid_game_path)
+        mock_detail_game_path = os.path.join(
+            self.resources_path, "mock_valid_game_detailed_info_page.html"
+        )
+        self.detail_game_html = read_file(mock_detail_game_path)
         mock_missing_game_path = os.path.join(
             self.resources_path, "mock_missing_game_page.html"
         )
@@ -27,7 +33,15 @@ class TestGetGameInfo(unittest.TestCase):
     @patch("scraping.extraction.requests.get")
     def test_valid_game_page(self, mock_get: MagicMock) -> None:
         """Test extracting info from a valid game page URL."""
-        mock_get.return_value = MagicMock(status_code=200, text=self.valid_game_html)
+
+        def return_sample_file_response(url):
+            responses = {
+                "https://www.notmetacritic.com/game/valid-game": self.valid_game_html,
+                "https://www.notmetacritic.com/game/valid-game/details": self.detail_game_html,
+            }
+            return MagicMock(status_code=200, content=responses[url])
+
+        mock_get.side_effect = return_sample_file_response
         game_info = get_game_info("https://www.notmetacritic.com/game/valid-game")
         self.assertIsNotNone(game_info)
         self.assertIsInstance(game_info, GameInfo)
@@ -35,21 +49,26 @@ class TestGetGameInfo(unittest.TestCase):
     @patch("scraping.extraction.requests.get")
     def test_invalid_url_format(self, mock_get: MagicMock) -> None:
         """Test handling of invalid URL format."""
-        with self.assertRaises(ValueError):
+        mock_get.return_value = MagicMock(
+            status_code=404, content=self.missing_game_html
+        )
+        with self.assertRaises(requests.exceptions.HTTPError):
             get_game_info("https://www.notmetacritic.com/game/invalid-url")
 
     @patch("scraping.extraction.requests.get")
     def test_non_existent_game_page(self, mock_get: MagicMock) -> None:
         """Test handling of non-existent game page URL."""
-        mock_get.return_value = MagicMock(status_code=404, text=self.missing_game_html)
-        with self.assertRaises(ValueError):
+        mock_get.return_value = MagicMock(
+            status_code=500, content=self.missing_game_html
+        )
+        with self.assertRaises(requests.exceptions.ConnectionError):
             get_game_info("https://www.notmetacritic.com/game/non-existent-game")
 
     @patch("scraping.extraction.requests.get")
     def test_empty_or_incomplete_game_page(self, mock_get: MagicMock) -> None:
         """Test handling of empty or incomplete game page."""
         mock_get.return_value = MagicMock(
-            status_code=200, text=self.incomplete_game_html
+            status_code=200, content=self.incomplete_game_html
         )
         with self.assertRaises(ValueError):
             get_game_info("https://www.notmetacritic.com/game/incomplete-game")
