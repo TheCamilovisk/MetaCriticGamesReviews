7a492e371f3e0da94d2dc7559439fd25d90ee05b
TheCamilovisk <camilolgon@gmail.com>
2024-03-05 19:21:21 -0300

    Adapting tests for the new reviews sample,

--- tests/test_user_reviews.py
+++ tests/test_user_reviews.py
@@ -1,11 +1,11 @@
+import json
 import os
 import unittest
+from copy import deepcopy
 from unittest.mock import MagicMock, patch
 
-import requests
-
 from scraping.extraction import get_game_reviews
-from tests.utils import get_resources_path, read_jsonlines_file
+from tests.utils import get_resources_path
 
 
 class TestGameReviews(unittest.TestCase):
@@ -19,7 +19,10 @@ class TestGameReviews(unittest.TestCase):
         mock_valid_response_path = os.path.join(
             self.resources_path, "mock_valid_response.jsonl"
         )
-        self.valid_response = read_jsonlines_file(mock_valid_response_path)
+        with open(mock_valid_response_path, "r") as file:
+            self.valid_response = json.load(file)
+        self.single_response = deepcopy(self.valid_response)
+        self.single_response["data"]["items"] = [self.single_response["data"]["items"][0]]
 
         self.valid_endpoint = "http://example.com/reviews"
         self.invalid_endpoint = "http://invalidurl.com"
@@ -30,7 +33,7 @@ class TestGameReviews(unittest.TestCase):
         Test fetching game reviews with a valid response containing multiple reviews.
         """
         mock_get.return_value.json = MagicMock(return_value=self.valid_response)
-        result = get_game_reviews(self.valid_endpoint)
+        result = [items for items in get_game_reviews(self.valid_endpoint)]
         self.assertEqual(len(result), 2, "Expected two reviews in the result")
 
     @patch("scraping.extraction.requests.get")
@@ -38,8 +41,9 @@ class TestGameReviews(unittest.TestCase):
         """
         Test fetching game reviews with a response containing a single review.
         """
-        mock_get.return_value.json = MagicMock(return_value=[self.valid_response[0]])
-        result = get_game_reviews(self.valid_endpoint)
+        print(self.single_response)
+        mock_get.return_value.json = MagicMock(return_value=self.single_response)
+        result = [items for items in get_game_reviews(self.valid_endpoint)]
         self.assertEqual(len(result), 1, "Expected a single review in the result")
 
     @patch("scraping.extraction.requests.get")
@@ -49,7 +53,7 @@ class TestGameReviews(unittest.TestCase):
         """
         mock_get.side_effect = Exception
         with self.assertRaises(Exception):
-            get_game_reviews(self.invalid_endpoint)
+            list(get_game_reviews(self.invalid_endpoint))
 
     @patch("scraping.extraction.requests.get")
     def test_invalid_json(self, mock_get: MagicMock) -> None:
@@ -58,7 +62,7 @@ class TestGameReviews(unittest.TestCase):
         """
         mock_get.return_value.json = MagicMock(side_effect=ValueError("Invalid JSON"))
         with self.assertRaises(ValueError):
-            get_game_reviews(self.valid_endpoint)
+            list(get_game_reviews(self.valid_endpoint))
 
     @patch("scraping.extraction.requests.get")
     def test_unexpected_json_structure(self, mock_get: MagicMock) -> None:
@@ -67,7 +71,7 @@ class TestGameReviews(unittest.TestCase):
         """
         mock_get.return_value.json = MagicMock(return_value=[{"unexpected": "structure"}])
         with self.assertRaises(ValueError):
-            get_game_reviews(self.valid_endpoint)
+            list(get_game_reviews(self.valid_endpoint))
 
 
 if __name__ == "__main__":
